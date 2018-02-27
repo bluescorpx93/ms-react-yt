9d71311b1fa75b91b19f9caf5dc24f039f8e7185
Toufiq <burningraven06@gmail.com>
2018-02-28 01:17:17 +0600

    create multiple components

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+export class Home extends React.Component{
+   render(){
+      return (
+         <div className='container'>
+            <div className='row'>
+               <h3> Hey Hey</h3>
+            </div>
+         </div>
+      );
+   }
+}
