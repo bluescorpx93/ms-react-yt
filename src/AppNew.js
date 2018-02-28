d2f2e2c67dadd33411b8cb66e20c956b5feba0f4
Toufiq <burningraven06@gmail.com>
2018-02-28 17:00:33 +0600

    setup routes

--- src/AppNew.js
+++ src/AppNew.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Root } from './RootComponent';
+import { User } from './components/user';
 import { HomeNew } from './components/homeNew';
-
+import {Route, BrowserRouter, Link } from 'react-router-dom';
 
 class AppNew extends Component {
   render(){
      return (
-        <Root>
-           <HomeNew />
-        </Root>
+        <BrowserRouter>
+        <div>
+           <Root/>
+           <Route path={"/user"} component={User}/>
+           <Route exact path={"/"} component={HomeNew}/>
+         </div>
+        </BrowserRouter>
+      //   <Root>
+      //      <HomeNew />
+      //   </Root>
      );
   }
 }
--- src/components/user.js
+++ src/components/user.js
@@ -1,5 +1,5 @@
 import React from 'react';
-export default class User extends React.Component{
+export class User extends React.Component{
    render(){
       return (
          <div> 
