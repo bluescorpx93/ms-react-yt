f77a5d4d0406d5dbe21700e681bc431f1766769a
Toufiq <burningraven06@gmail.com>
2018-02-28 16:10:09 +0600

    explore

--- src/App.js
+++ src/App.js
@@ -8,11 +8,42 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      homeLinkAC: "Home"
+      homeLinkAC: "Home",
+      homeMounted: true
     }
     this.onChangeLinkName = this.onChangeLinkName.bind(this);
+    console.log("Constructor");
+
   }
-  
+
+  onChangeHomeMounted(){
+    this.setState({
+       homeMounted: !this.state.homeMounted
+    })
+  }
+
+  componentWillMount(){
+    console.log("comp will mount");
+  }
+  componentDidMount(){
+      console.log('comp did mount');
+  }
+  componentWillReceiveProps(nextProps){
+      console.log('comp will rec props ', nextProps);
+  }
+  shouldComponentUpdate(nextProps, nextState){
+      console.log('should comp update ', nextProps, nextState)
+      return true;
+  }
+  componentWillUpdate(nextProps, nextState){
+      console.log('comp will update ', nextProps, nextState)
+  }
+  componentDidUpdate(prevProps, prevState){
+      console.log('comp did update ', prevProps, prevState)
+  }
+  componentWillUnmount(){
+      console.log('comp will unmount');
+  }
   onGreet(){
     console.log("Say Hey From Mother Component");
   }
@@ -24,7 +55,18 @@ class App extends Component {
   }
 
   render() {
-
+    let homeComp = "";
+    if (this.state.homeMounted){
+      homeComp = (
+      <Home 
+        name={"Max"} 
+        initialAge={27}
+        greet={this.onGreet}
+        changeLink={this.onChangeLinkName}
+        initialLinkName={this.state.homeLinkAC}
+      />
+    );
+    }
     return (
       <div className='container'>
         <div className='row'>
@@ -32,14 +74,10 @@ class App extends Component {
             <Header homeLink={this.state.homeLinkAC}/>
           </div>
           <div className='col-xs-10 col-xs-offset-1'>
-            <Home 
-              name={"Max"} 
-              initialAge={27}
-              greet={this.onGreet}
-              changeLink={this.onChangeLinkName}
-            />
-              <p> This is a Child Prop</p>
-            <Home />
+          {homeComp}
+            <button onClick={this.onChangeHomeMounted.bind(this)}className='btn btn-primary'>(Un)Mount Home</button>
+              {/* <p> This is a Child Prop</p> */}
+            {/* <Home /> */}
           </div>
         </div>
       </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,13 +3,15 @@ import PropTypes from 'prop-types';
 
 export class Home extends React.Component{
    constructor(props){
-      super();
+      super(props);
       this.state = {
          age: props.initialAge,
-         homeLinkHC: "Changed Link"
+         homeLinkHC: props.initialLinkName,
+
       }
       this.onMakeAgeOlder = this.onMakeAgeOlder.bind(this);
       this.onChangeName = this.onChangeName.bind(this);
+      this.onHandleChange = this.onHandleChange.bind(this);
    }
    
    onMakeAgeOlder(){
@@ -22,17 +24,25 @@ export class Home extends React.Component{
       this.props.changeLink(this.state.homeLinkHC)
    }
 
+   onHandleChange(event){
+      this.setState({
+         homeLinkHC: event.target.value
+      })
+   }
+
    render(){
-      console.log(this.pros);
+      
       return (
          <div className='container'>
             <div className='row'>
                <h3> Hey Hey</h3>
                <p>Name: {this.props.name}</p>
                <p>Age:{this.state.age}</p>
-               <p>{this.props.children}</p>
+               {/* <p>{this.props.children}</p> */}
                <button onClick={this.onMakeAgeOlder} className='btn btn-primary'>Inc Age</button>
                <button onClick={this.props.greet} className='btn btn-default'>Greet</button>
+               <hr/>
+               <input type='text' className='form-control' defaultValue={this.props.initialLinkName} onChange={this.onHandleChange} />
                <button onClick ={this.onChangeName} className='btn btn-warning'> Change Name</button>
             </div>
          </div>
@@ -43,5 +53,6 @@ export class Home extends React.Component{
 Home.propTypes = {
    name: PropTypes.string,
    age: PropTypes.number,
-   greet: PropTypes.func
+   greet: PropTypes.func,
+  
 }
